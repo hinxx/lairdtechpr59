6afddac4b3623d9f6effb970af1075f9e228ff2f
Hinko Kocevar <hinko.kocevar@esss.se>
2016-12-19 10:53:17 +0100

    EEE makefile.
    Fix startup snippet.

--- GNUmakefile
+++ GNUmakefile
@@ -4,10 +4,7 @@ include ${EPICS_ENV_PATH}/module.Makefile
 
 EXCLUDE_ARCHS += eldk
 
-USR_DEPENDENCIES += stream
 USR_DEPENDENCIES += asyn
-
-USR_CXXFLAGS += -fno-operator-names
 
 SOURCES =
 
--- startup/lairdtechPR59.cmd
+++ startup/lairdtechPR59.cmd
@@ -1,27 +1,32 @@
 require lairdtechpr59
 
-epicsEnvSet("PREFIX",   "PR59")
-
-#var streamDebug 1
-
-epicsEnvSet("SERIAL_PORT", "/dev/ttyUSB0")
+epicsEnvSet("DEVICE",      "/dev/ttyUSB0")
+epicsEnvSet("SERIAL_PORT", "LT59_SERIAL")
+epicsEnvSet("PORT",        "LT59")
 
 # drvAsynSerialPortConfigure(port, ttyName, priority, noAutoConnect, noProcessEosIn)
-drvAsynSerialPortConfigure("$(PREFIX)", "$(SERIAL_PORT)", 0, 0, 0)
-asynSetOption("$(PREFIX)", 0, "baud",   "115200")
-asynSetOption("$(PREFIX)", 0, "bits",   "8")
-asynSetOption("$(PREFIX)", 0, "parity", "none")
-asynSetOption("$(PREFIX)", 0, "stop",   "1")
-asynSetOption("$(PREFIX)", 0, "clocal", "Y")
-asynSetOption("$(PREFIX)", 0, "crtscts","N")
+drvAsynSerialPortConfigure("$(SERIAL_PORT)", "$(DEVICE)", 0, 0, 0)
+asynSetOption("$(SERIAL_PORT)", 0, "baud",   "115200")
+asynSetOption("$(SERIAL_PORT)", 0, "bits",   "8")
+asynSetOption("$(SERIAL_PORT)", 0, "parity", "none")
+asynSetOption("$(SERIAL_PORT)", 0, "stop",   "1")
+asynSetOption("$(SERIAL_PORT)", 0, "clocal", "Y")
+asynSetOption("$(SERIAL_PORT)", 0, "crtscts","N")
 
-# handled by the Terminator = CR; in the proto file
-#asynOctetSetInputEos( "$(PREFIX)", 0, "\r")
-#asynOctetSetOutputEos("$(PREFIX)", 0, "\r")
+asynOctetSetInputEos("$(SERIAL_PORT)", 0, "\r\n")
+asynOctetSetOutputEos("$(SERIAL_PORT)", 0, "\r")
 
-asynSetTraceIOMask("$(PREFIX)",0,0xff)
-#asynSetTraceMask("$(PREFIX)",0,0xff)
+#asynSetTraceIOMask("$(SERIAL_PORT)",0,0xff)
+#asynSetTraceMask("$(SERIAL_PORT)",0,0xff)
+
+# LTPR59Configure(const char *portName, const char *serialPort);
+LTPR59Configure($(PORT), $(SERIAL_PORT))
+#asynSetTraceIOMask("$(PORT)",0,0xff)
+#asynSetTraceMask("$(PORT)",0,0xff)
 
 # Load record instances
-dbLoadRecords("lairdtechPR59.template","P=$(PREFIX):,R=,PORT=$(PREFIX)")
-dbLoadRecords("asynRecord.db",         "P=$(PREFIX):,R=asyn,PORT=$(PREFIX),ADDR=0,OMAX=100,IMAX=100")
+dbLoadRecords("lairdtechPR59_main.template",  "P=$(PORT):,R=,PORT=$(PORT),SERIAL_PORT=$(SERIAL_PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("lairdtechPR59_pid.template",   "P=$(PORT):,R=,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("lairdtechPR59_temp.template",  "P=$(PORT):,R=,T=1,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+# TODO: Add support for other temperature sensors
+dbLoadRecords("asynRecord.db","P=$(PORT):,R=asyn,PORT=$(PORT),ADDR=0,OMAX=100,IMAX=100")
